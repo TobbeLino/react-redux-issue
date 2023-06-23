5b475c7c6fbaa87014a0ddb1262c1bb2ceb66cf2
Tobias Lind <tobias@vergic.com>
2023-06-23 13:51:41 +0200

    Commented out "hacky fix"

--- src/Main_1.js
+++ src/Main_1.js
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';          // <--- If this (only) is imported here, Main_2 will start working (both <LangProvider> and <GlobalStyles>)
+// import styled from '@emotion/styled';          // <--- If this (only) is imported here, Main_2 will start working (both <LangProvider> and <GlobalStyles>)
 // import { LangProvider } from './LangProvider'; // <--- If this (only) is imported here, <LangProvider> in Main_2 will start working (but not <GlobalStyles>)
 
 export const Main_1 = () => {
